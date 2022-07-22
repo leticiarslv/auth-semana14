c3aaf2bb633eee82c50e47d4874a64423eaa9d1a
Letícia Rodrigues <leticiab.rodrigues@outlook.com>
2022-07-21 21:12:38 -0300

    exercicio finalizado - autenticação&autorização

---
+++ semana14/S13-Projeto-3-CRUD-BD/server.js
@@ -0,0 +1,7 @@
+// 1. importa o app(api)
+// 2. definir uma porta e ouvi-la
+const app = require('./src/app')
+
+const PORT = process.env.PORT
+
+app.listen(8000, () => console.log(`conectado! ${PORT}`))
